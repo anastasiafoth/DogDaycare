e6ff888fd55996d6002155809ec2c0815e253775
anastasiafoth <anastasiafoth9@gmail.com>
2026-02-17 13:58:01 +0100

    Added form and a submit handler, which adds the new input to list, adding it to state

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import Dogs from "./Dogs";
 import "./App.css";
 
 function App() {
-
   const [dogs, setDogs] = useState(["Rover", "Buddy", "Rex"]);
-  const [showDog, setShowDogs] = useState(true)
+  const [showDog, setShowDogs] = useState(true);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const newDog = formData.get("newName");
+    setDogs((prevDogList) => [...prevDogList, newDog]);
+
+    event.currentTarget.reset();
+  }
 
   return (
     <div>
       <h2>Dog Daycare</h2>
-      {showDog && <Dogs dogs={dogs}/>}
+      <form onSubmit={handleSubmit} className="add-new-dog">
+        <input type="text" placeholder="New dog name" name="newName" />
+        <button>Add</button>
+      </form>
+      {showDog && <Dogs dogs={dogs} />}
     </div>
   );
 }
